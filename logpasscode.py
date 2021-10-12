e67b1c5245e9daba9a366e6a29c51eb981c859cf
Ogabek <ogabeksherakhmatov@gmail.com>
2021-10-12 09:56:39 +0500

    Step 3. Is login exists is ready

--- logpasscode.py
+++ logpasscode.py
@@ -1,4 +1,14 @@
 import os
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    database='user__data',
+    user='Abdulaziz',
+    password='123123123',
+)
+mycursor = mydb.cursor()
+
 
 class Login_password:
 
@@ -39,32 +49,25 @@ class Login_password:
         self.registration_text()
 
         self.username = input("Enter your username: ").lower().strip()
-        while not self.username.isalpha():
+        while not self.username.isalpha() or len(self.username) > 30:
             self.clear_everything()
             self.registration_text()
-            self.username = input("Invalid input. Username only contains letters: ").lower().strip()
-
-
+            self.username = input(
+                "Invalid input. Username only contains letters or maximum length should be 30 ").lower().strip()
 
         self.login = input("Enter your login: ").lower().strip()
-        while len(self.login) < 6:
+        while len(self.login) < 6 or len(self.login) > 30 or self.is_login_exists(self.login):
             self.clear_everything()
-            self.login = input("Enter your login: ").lower().strip()
-
-
+            self.login = input("Invalid input. 6 <= login <= 30: ").lower().strip()
 
         self.password = input("Enter your password: ").lower().strip()
-        while len(self.password) < 8:
+        while len(self.password) < 8 or len(self.password) > 30:
             self.clear_everything()
-            self.password = input("Invalid password. It should be at least 8: ").lower().strip()
-
+            self.password = input("Invalid password. It should be at least 8, maximum 30: ").lower().strip()
 
         self.age = input("Enter your age: ").strip()
         while not self.age.isnumeric() or int(self.age) > 200:
             self.age = input("Invalid age. Enter it again: ").strip()
-
-
-
 
     def log_in(self):
         print('log in')
@@ -90,10 +93,16 @@ class Login_password:
     def clear_everything():
         os.system('clear')
 
-
     @staticmethod
     def registration_text():
         print("<<< Registration page >>>")
+
+
+    def is_login_exists(self,input_login):
+        mycursor.execute(f"select * from users  where login='{input_login}'")
+        data_users = mycursor.fetchall()
+
+        return data_users
 
 
 logpass = Login_password()
